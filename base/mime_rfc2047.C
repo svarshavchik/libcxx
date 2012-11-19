930f8dcd5d78d7fdb861d98558c1121961733fb3
Sam Varshavchik <mrsam@courier-mta.com>
2012-11-18 21:54:45 -0500

    RFC 2047 encoding and decoding.

---
+++ base/mime_rfc2047.C
@@ -0,0 +1,547 @@
+/*
+** Copyright 2012 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+
+#include "libcxx_config.h"
+#include "x/mime/rfc2047.H"
+#include "x/mime/headeriter.H"
+#include "x/tostring.H"
+#include "x/iconviofilter.H"
+#include "x/qp.H"
+#include "x/base64.H"
+#include "x/chrcasecmp.H"
+#include "x/exception.H"
+
+#include <sstream>
+
+namespace LIBCXX_NAMESPACE {
+	namespace mime {
+#if 0
+	};
+};
+#endif
+
+#define UTF8 "UTF-8"
+
+#define IS_UTF8TAIL(c)  (((int)(c) & 0xC0) == 0x80)
+
+rfc2047_encode_base::rfc2047_encode_base()
+{
+}
+
+rfc2047_encode_base::~rfc2047_encode_base() noexcept
+{
+}
+
+class LIBCXX_HIDDEN rfc2047_std_encoding : public rfc2047_encode_base {
+ public:
+
+	rfc2047_std_encoding() {}
+	~rfc2047_std_encoding() noexcept {}
+
+	bool do_encode(char32_t c)
+	{
+		return false;
+	}
+};
+
+std::string to_rfc2047(const std::u32string &str)
+{
+	return to_rfc2047("", str);
+}
+
+std::string to_rfc2047(const std::string &language,
+		       const std::u32string &str)
+{
+#pragma GCC visibility push(hidden)
+
+	rfc2047_std_encoding encoder;
+
+	encoder.encode(language, str);
+
+	return encoder.formatted_string;
+#pragma GCC visibility pop
+}
+
+// ------------------------------------------------------------------------
+
+#define WHITESPACE(c) ((c) == ' ' || (c) == '\t')
+
+std::u32string::const_iterator
+rfc2047_encode_base::next_word(std::u32string::const_iterator b,
+			       std::u32string::const_iterator e,
+			       bool &encode_it)
+{
+	encode_it=false;
+
+	while (b != e)
+	{
+		if (WHITESPACE(*b))
+			break;
+		if (do_encode(*b) || *b < ' ' || *b > 127)
+			encode_it=true;
+		++b;
+	}
+
+	return b;
+}
+
+std::string
+rfc2047_encode_base::do_encode(const std::string &utf8_str,
+			       size_t maximum_length,
+			       const std::string &language)
+{
+	std::string qp=do_qp(utf8_str, maximum_length, language);
+	std::string b64=do_base64(utf8_str, maximum_length, language);
+
+	return qp.size() < b64.size() ? qp:b64;
+}
+
+class LIBCXX_HIDDEN rfc2047_encode_base::qp_traits {
+
+ public:
+	rfc2047_encode_base &base;
+
+	qp_traits(rfc2047_encode_base &baseArg) : base(baseArg) {}
+
+	bool encode(char c)
+	{
+		return c == '_' || c == '?'
+			|| (unsigned char)c < ' '
+			|| (c <= 0x7F && base.do_encode(c));
+	}
+};
+
+static void enc_emit(std::ostringstream &o,
+		     std::string::iterator b,
+		     std::string::iterator e,
+		     const char *&sep,
+		     const char method,
+		     const std::string &language)
+{
+	std::replace(b, e, ' ', '_');
+	o << sep << "=?" UTF8;
+
+	if (language.size())
+		o << "*" << language;
+
+	o << "?" << method << '?' << std::string(b, e) << "?=";
+	sep=" ";
+}
+
+static void qp_emit(std::ostringstream &o,
+		    std::string::iterator b,
+		    std::string::iterator e,
+		    const char *&sep,
+		    const std::string &language)
+{
+	enc_emit(o, b, e, sep, 'Q', language);
+}
+
+static void b64_emit(std::ostringstream &o,
+		     std::string::const_iterator b,
+		     std::string::const_iterator e,
+		     const char *&sep,
+		     const std::string &language)
+{
+	std::string encoded;
+
+	std::copy(b, e,
+		  base64<>::encoder<std::back_insert_iterator<std::string>
+		  >(std::back_insert_iterator<std::string>(encoded), 0)).eof();
+
+	enc_emit(o, encoded.begin(), encoded.end(), sep, 'B', language);
+}
+
+std::string rfc2047_encode_base::do_qp(const std::string &utf8_str,
+				       size_t maximum_length,
+				       const std::string &language)
+{
+	std::ostringstream o;
+
+	std::string buf;
+
+	const char *sep="";
+
+	auto encoder=
+		qp_encoder<std::back_insert_iterator<std::string>,
+			   qp_traits>(std::back_insert_iterator<
+				      std::string>(buf), 0, false,
+				      qp_traits(*this));
+
+	size_t i=0;
+
+	for (char c:utf8_str)
+	{
+		if (!IS_UTF8TAIL(c))
+		{
+			if (buf.size() > maximum_length)
+			{
+				qp_emit(o, buf.begin(), buf.begin()+i, sep,
+					language);
+				buf=buf.substr(i);
+			}
+			i=buf.size();
+		}
+		*encoder++=c;
+	}
+
+	if (buf.begin() != buf.end())
+		qp_emit(o, buf.begin(), buf.end(), sep, language);
+	return o.str();
+}
+
+std::string rfc2047_encode_base::do_base64(const std::string &utf8_str,
+					   size_t maximum_length,
+					   const std::string &language)
+{
+	std::ostringstream o;
+	const char *sep="";
+	size_t start=0;
+	size_t i=0;
+
+	size_t n;
+
+	for (n=0; n<utf8_str.size(); ++n)
+	{
+		if (!IS_UTF8TAIL(utf8_str[n]))
+		{
+			if ( (n-start) > maximum_length / 4 * 3)
+			{
+				b64_emit(o, utf8_str.begin()+start,
+					 utf8_str.begin()+i, sep,
+					 language);
+				start=i;
+			}
+			i=n;
+		}
+	}
+
+	if (start < n)
+		b64_emit(o, utf8_str.begin()+start, utf8_str.begin()+n, sep,
+			 language);
+	return o.str();
+}
+
+std::string rfc2047_encode_base::to_utf8(std::u32string::const_iterator b,
+					 std::u32string::const_iterator e)
+{
+	return iconviofilter::from_u32string(std::u32string(b, e), UTF8);
+}
+
+void rfc2047_encode_base::encode(const std::string &language,
+				 const std::u32string &text)
+{
+	std::ostringstream o;
+
+	std::u32string::const_iterator b=text.begin(), e=text.end();
+
+	size_t overhead=sizeof("=?" UTF8 "?Q?" "?=")
+		+ (language.size() ? language.size()+1:0);
+
+	if (overhead > 50)
+		overhead=50; // GIGO
+
+	size_t maximum_length=70-overhead;
+
+	while (b != e)
+	{
+		if (WHITESPACE(*b))
+		{
+			o << (char)*b;
+			++b;
+			continue;
+		}
+
+		std::u32string::const_iterator p=b;
+		bool encode_it;
+
+		b=next_word(b, e, encode_it);
+
+		if (language.size())
+			encode_it=true;
+
+		if (encode_it)
+		{
+			// If the following word will also get encoded,
+			// it must be merged together with this word.
+
+			std::u32string::const_iterator q=b;
+
+			while (b != e)
+			{
+				// Find the start of the next word.
+				if (WHITESPACE(*b))
+				{
+					++b;
+					continue;
+				}
+
+				b=next_word(b, e, encode_it);
+
+				if (language.size())
+					encode_it=true;
+
+				if (!encode_it)
+					break;
+				q=b;
+			}
+			b=q;
+
+			o << do_encode(to_utf8(p, b), maximum_length,
+				       language);
+			continue;
+		}
+
+		std::string s=to_utf8(p, b);
+
+		o << (s.size() > maximum_length ?
+		      do_encode(s, maximum_length, language):s);
+	}
+	formatted_string=o.str();
+}
+
+// --------------------------------------------------------------------------
+
+std::u32string from_rfc2047(const std::string &string,
+			    const std::string &native_charset)
+{
+	return from_rfc2047_lang(string, native_charset).second;
+}
+
+std::pair<std::string, std::u32string>
+from_rfc2047_lang(const std::string &string,
+		  const std::string &native_charset)
+{
+	std::vector<std::pair<std::string, std::u32string> > res;
+
+	from_rfc2047(string, native_charset, res);
+
+	std::pair<std::string, std::u32string> ret;
+
+	size_t size=0;
+
+	for (const auto &fragment:res)
+	{
+		if (fragment.first.size() && !ret.first.size())
+			ret.first=fragment.first; // Capture language.
+
+		size += fragment.second.size();
+	}
+
+	ret.second.reserve(size);
+
+	for (const auto &fragment:res)
+	{
+		ret.second += fragment.second;
+	}
+
+	return ret;
+}
+
+static void transcode(const std::string &str,
+		      std::u32string &ret,
+		      const std::string &charset)
+{
+	try {
+		ret=iconviofilter::to_u32string(str, charset);
+	} catch (const x::exception &e)
+	{
+		std::stringstream o;
+
+		o << "[" << e << "]";
+
+		ret=std::u32string(std::istreambuf_iterator<char>(o),
+				   std::istreambuf_iterator<char>());
+	}
+}
+
+// Does something stats with =? ?
+
+static inline bool is_start_of_encoded_word(std::string::const_iterator b,
+					    std::string::const_iterator e)
+{
+	if (b != e && *b++ == '=')
+	{
+		if (b != e && *b == '?')
+			return true;
+	}
+	return false;
+}
+
+// Something starting with =?
+
+static inline std::string::const_iterator
+decode_next_word(std::string::const_iterator b,
+		 std::string::const_iterator e,
+		 std::string &next_lang,
+		 std::u32string &next_str,
+		 const std::string &native_charset)
+{
+	auto start=b;
+
+	b += 2;
+
+	auto q=std::find(b, e, '?');
+
+	std::string charset=std::string(b, q);
+
+	auto c_b=charset.begin(), c_e=charset.end(),
+		c_asterisk=std::find(c_b, c_e, '*');
+
+	if (c_asterisk != c_e) // RFC 2231 extension, language indicator
+	{
+		next_lang=std::string(c_asterisk+1, c_e);
+		std::transform(next_lang.begin(),
+			       next_lang.end(),
+			       next_lang.begin(),
+			       std::ptr_fun(chrcasecmp::toupper));
+		charset=std::string(c_b, c_asterisk);
+	}
+
+	b=q;
+	if (b != e && ++b != e)
+	{
+		char method=*b;
+
+		b=std::find(b, e, '?');
+		if (b != e)
+		{
+			auto encoded_begin= ++b;
+
+			char prev=0;
+
+			typedef std::back_insert_iterator<std::string> ins_iter;
+
+			typedef qp_decoder<ins_iter> qp_t;
+
+			while (b != e)
+			{
+				if (*b == '=' && prev == '?')
+				{
+					std::string s;
+
+					switch (method) {
+					default:
+						break;
+					case 'b':
+					case 'B':
+						base64<>::decode(encoded_begin,
+								 b-1,
+								 ins_iter(s));
+						transcode(s, next_str, charset);
+						return ++b;
+
+					case 'q':
+					case 'Q':
+
+						auto qp=qp_t(ins_iter(s));
+
+						--b;
+						while (encoded_begin != b)
+						{
+							char c=*encoded_begin;
+
+							if (c == '_')
+								c=' ';
+
+							*qp++=c;
+							++encoded_begin;
+						}
+
+						transcode(s, next_str, charset);
+						return b+2;
+					}
+					break;
+				}
+
+				prev= *b;
+				++b;
+			}
+		}
+	}
+
+	// We couldn't parse it, so pass through everything we skipped.
+
+	transcode(std::string(start, b), next_str, native_charset);
+	return b;
+}
+
+void from_rfc2047(const std::string &string,
+
+		  const std::string &native_charset,
+
+		  std::vector<std::pair<std::string, std::u32string> > &res)
+{
+	bool previous_word_was_decoded=false;
+
+	std::string::const_iterator b=string.begin(), e=string.end();
+
+	while (b != e)
+	{
+		auto p=std::find_if(b, e, []
+				    (char c)
+				    {
+					    return !WHITESPACE(c);
+				    });
+		auto s=p;
+
+		bool next_word_is_encoded=is_start_of_encoded_word(p, e);
+
+		if (previous_word_was_decoded && next_word_is_encoded)
+			b=p; // Drop whitespace between encoded words.
+
+		std::string next_lang;
+		std::u32string next_str;
+
+		if (!next_word_is_encoded)
+		{
+			p=std::find_if(s, e, []
+				       (char c)
+				       {
+					       return WHITESPACE(c);
+				       });
+
+			transcode(std::string(b, p), next_str, native_charset);
+			b=p;
+		}
+		else if (s != b) // Whitespace before 1st encoded word
+		{
+			transcode(std::string(b, s), next_str, native_charset);
+			next_word_is_encoded=false;
+			b=s;
+		}
+		else
+		{
+			b=decode_next_word(b, e, next_lang, next_str,
+					   native_charset);
+		}
+
+		previous_word_was_decoded=next_word_is_encoded;
+
+		// Two strings in a row, same language, combine them.
+
+		if (!res.empty() && res.back().first == next_lang)
+		{
+			res.back().second += next_str;
+		}
+		else
+			res.push_back(std::make_pair(next_lang, next_str));
+	}
+}
+
+std::string from_rfc2047_as_utf8(const std::string &string,
+				 const std::string &native_charset)
+{
+	return iconviofilter::from_u32string(from_rfc2047(string,
+							  native_charset),
+				      "UTF-8");
+}
+
+#if 0
+{
+	{
+#endif
+	}
+}
---
+++ base/testmimerfc2047.C
@@ -0,0 +1,200 @@
+/*
+** Copyright 2012 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+
+#include "libcxx_config.h"
+#include "x/options.H"
+#include "x/mime/rfc2047.H"
+#include "x/iconviofilter.H"
+#include "x/exception.H"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
+void testmimerfc2047()
+{
+	static const struct {
+		const char *utf8;
+		const char *rfc2047;
+	} tests[] = {
+		{
+			"Hello world",
+			"Hello world",
+		},
+
+		{
+			"Hello w\xc3\xb3rld",
+			"Hello =?UTF-8?B?d8Ozcmxk?=",
+		},
+
+		{
+			"Hello w\xc3\xb3rld Hello",
+			"Hello =?UTF-8?B?d8Ozcmxk?= Hello",
+		},
+
+		{
+			"Hello w\xc3\xb3rld w\xc3\xb3rld",
+			"Hello =?UTF-8?B?d8OzcmxkIHfDs3JsZA==?=",
+		},
+
+		{
+			"Hello w\xc3\xb3rldworld",
+			"Hello =?UTF-8?Q?w=C3=B3rldworld?=",
+		},
+
+		{
+			"Hello w\xc3\xb3rldworld w\xc3\xb3rldworld",
+			"Hello =?UTF-8?Q?w=C3=B3rldworld_w=C3=B3rldworld?=",
+		},
+
+		{
+			"Hello w\xc3\xb3rldworldworld=w\xc3\xb3rldworld",
+			"Hello =?UTF-8?Q?w=C3=B3rldworldworld=3Dw=C3=B3rldworld?=",
+		},
+
+		{
+			"Hello w\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3"
+			"\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3rld",
+			"Hello =?UTF-8?B?d8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7Ny?= =?UTF-8?B?bGQ=?=",
+		},
+
+		{
+			"Hello w\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3"
+			"\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3rld",
+			"Hello =?UTF-8?B?d8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7M=?= =?UTF-8?B?w7NybGQ=?=",
+		},
+
+		{
+			"Heello w\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3"
+			"\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3rld",
+			"Heello =?UTF-8?B?d8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7M=?= =?UTF-8?B?w7NybGQ=?=",
+		},
+
+		{
+			"Heeello w\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3"
+			"\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3\xc3\xb3rld",
+			"Heeello =?UTF-8?B?d8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7PDs8Ozw7M=?= =?UTF-8?B?w7NybGQ=?=",
+		},
+
+		{
+			"Hello 012345678901234567890123456789012345678901234\xc3\xb3\xc3\xb3" "678901234567890",
+			"Hello =?UTF-8?Q?012345678901234567890123456789012345678901234=C3=B3=C3=B3?= =?UTF-8?Q?678901234567890?=",
+		},
+
+		{
+			"Hello 0123456789012345678901234567890123456789012345\xc3\xb3\xc3\xb3" "678901234567890",
+			"Hello =?UTF-8?Q?0123456789012345678901234567890123456789012345=C3=B3?= =?UTF-8?Q?=C3=B3678901234567890?=",
+		},
+
+		{
+			"Hello 01234567890123456789012345678901234567890123456\xc3\xb3\xc3\xb3" "78901234567890",
+			"Hello =?UTF-8?Q?01234567890123456789012345678901234567890123456=C3=B3?= =?UTF-8?Q?=C3=B378901234567890?=",
+		},
+
+		{
+			"Hello 012345678901234567890123456789012345678901234567\xc3\xb3\xc3\xb3" "8901234567890",
+			"Hello =?UTF-8?Q?012345678901234567890123456789012345678901234567=C3=B3?= =?UTF-8?Q?=C3=B38901234567890?=",
+		},
+
+		{
+			"Hello 0123456789012345678901234567890123456789012345678\xc3\xb3\xc3\xb3" "901234567890",
+			"Hello =?UTF-8?Q?0123456789012345678901234567890123456789012345678=C3=B3?= =?UTF-8?Q?=C3=B3901234567890?=",
+		},
+
+		{
+			"Hello 01234567890123456789012345678901234567890123456789\xc3\xb3\xc3\xb3" "01234567890",
+			"Hello =?UTF-8?Q?01234567890123456789012345678901234567890123456789=C3=B3?= =?UTF-8?Q?=C3=B301234567890?=",
+		},
+
+		{
+			"Hello 012345678901234567890123456789012345678901234567890\xc3\xb3\xc3\xb3" "1234567890",
+			"Hello =?UTF-8?Q?012345678901234567890123456789012345678901234567890=C3=B3?= =?UTF-8?Q?=C3=B31234567890?=",
+		},
+
+		{
+			"Hello 0123456789012345678901234567890123456789012345678901\xc3\xb3\xc3\xb3" "234567890",
+			"Hello =?UTF-8?Q?0123456789012345678901234567890123456789012345678901?= =?UTF-8?Q?=C3=B3=C3=B3234567890?=",
+		},
+	};
+	for (const auto &test:tests)
+	{
+		std::string utf8=test.utf8;
+
+		std::string rfc2047=LIBCXX_NAMESPACE::mime
+			::to_rfc2047_utf8(utf8);
+
+		if (rfc2047 != test.rfc2047)
+		{
+			throw EXCEPTION("Got:      " + rfc2047 + "\n"
+					"Expected: " + test.rfc2047);
+		}
+
+		std::string orig=
+			LIBCXX_NAMESPACE::mime::from_rfc2047_as_utf8(rfc2047,
+								     "UTF-8");
+
+		if (orig != test.utf8)
+			throw EXCEPTION("Got:      " + orig + "\n"
+					"Expected: " + test.utf8);
+	}
+
+	std::string rfc2047=LIBCXX_NAMESPACE::mime
+		::to_rfc2047_lang_utf8("EN", "Hello world");
+
+	if (rfc2047 != "=?UTF-8*EN?Q?Hello_world?=")
+		throw EXCEPTION("RFC 2231 extension does not work");
+
+	rfc2047=LIBCXX_NAMESPACE::mime::from_rfc2047_as_utf8("=?UTF-8?Q?=invalid=?", "UTF-8");
+	std::cout << "Just for kicks: " << rfc2047 << std::endl;
+
+	auto pair=LIBCXX_NAMESPACE::mime::from_rfc2047_lang("=?iso-8859-1*en?q?O=A0k?=", "UTF-8");
+
+	if (pair.first != "EN")
+		throw EXCEPTION("Did not get EN");
+
+	if (LIBCXX_NAMESPACE::iconviofilter::from_u32string(pair.second, "UTF-8")
+	    != "O\xc2\xa0k")
+		throw EXCEPTION("Did not transcode");
+
+	std::cout <<
+		LIBCXX_NAMESPACE::mime::from_rfc2047_as_utf8("foo\xA0" "bar",
+							     "UTF-8")
+		  << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+	try {
+		LIBCXX_NAMESPACE::option::list
+			options(LIBCXX_NAMESPACE::option::list::create());
+
+		options->addDefaultOptions();
+
+		LIBCXX_NAMESPACE::option::parser
+			opt_parser(LIBCXX_NAMESPACE::option::parser::create());
+
+		opt_parser->setOptions(options);
+		int err=opt_parser->parseArgv(argc, argv);
+
+		if (err == 0) err=opt_parser->validate();
+
+		if (err)
+		{
+			if (err == LIBCXX_NAMESPACE::option::parser::base
+			    ::err_builtin)
+				exit(0);
+
+			std::cerr << opt_parser->errmessage();
+			std::cerr.flush();
+			exit(1);
+		}
+		testmimerfc2047();
+	} catch (LIBCXX_NAMESPACE::exception &e)
+	{
+		std::cerr << e << std::endl;
+		exit(1);
+	}
+	return (0);
+}
