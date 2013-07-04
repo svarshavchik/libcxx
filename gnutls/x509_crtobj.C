2efb8b02424e2c48eba3dfcdc6ca820890d3fb79
Sam Varshavchik <mrsam@courier-mta.com>
2013-07-04 14:54:44 -0400

    GnuTLS 3 API change

--- gnutls/x509_crtobj.C
+++ gnutls/x509_crtobj.C
@@ -283,7 +283,7 @@ void gnutls::x509::crtObj::get_basic_constraints(bool &ca,
 						 int &pathLenConstraint)
 	const
 {
-	int caRet;
+	unsigned int caRet;
 	unsigned criticalRet;
 
 	chkerr(gnutls_x509_crt_get_basic_constraints(crt, &criticalRet,
@@ -466,7 +466,7 @@ void gnutls::x509::crtObj::get_all_extensions_list(std::list< std::pair<std::str
 
 	while (1)
 	{
-		int critflag=0;
+		unsigned int critflag=0;
 		size_t buf_s=0;
 		int rc=gnutls_x509_crt_get_extension_info(crt, indx,
 							  NULL, &buf_s,
