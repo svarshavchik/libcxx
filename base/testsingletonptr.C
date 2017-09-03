b09a13e52623448453d599620a2d0827ca211cc5
Sam Varshavchik <mrsam@courier-mta.com>
2017-09-03 08:11:23 -0400

    Add singletonptr.

---
+++ base/testsingletonptr.C
@@ -0,0 +1,21 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+
+#include "libcxx_config.h"
+#include "testsingletonptr.H"
+#include <iostream>
+#include <cstdlib>
+
+extern int testsingletonptr();
+
+int main()
+{
+	if (testsingletonptr() != 4)
+	{
+		std::cerr << "singletonptr test failed." << std::endl;
+		exit(1);
+	}
+	return 0;
+}
---
+++ base/testsingletonptr2.C
@@ -0,0 +1,34 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+
+#include "libcxx_config.h"
+#include "testsingletonptr.H"
+
+
+int testsingletonptr()
+{
+	typedef LIBCXX_NAMESPACE::singletonptr<testSingletonPtrObj> global;
+
+	int x=0;
+
+	{
+		global glob{LIBCXX_NAMESPACE::ref<testSingletonPtrObj>::create()};
+
+		if (!glob)
+			return x;
+
+		global glob2;
+
+		if (glob2)
+			x=glob2->x;
+	}
+
+	global glob3;
+
+	if (!glob3)
+		return x;
+
+	return 0;
+}
