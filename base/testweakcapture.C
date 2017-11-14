0c2c19a49220dd4dfe0d1bc66f0546d8c78eb175
Sam Varshavchik <mrsam@courier-mta.com>
2017-11-13 19:47:00 -0500

    Reimplement make_weak_capture() using C++17. RPM fixes.

--- base/testweakcapture.C
+++ base/testweakcapture.C
@@ -1,19 +1,28 @@
 #include "x/weakcapture.H"
 #include <iostream>
 #include <cstdlib>
+#include <type_traits>
 
 template<typename foo> int Foo(const foo &f)
 {
 	int val=0;
 
-	if (f.get([&val]
-		  (const LIBCXX_NAMESPACE::ref<LIBCXX_NAMESPACE::obj> &a,
-		   const LIBCXX_NAMESPACE::ref<LIBCXX_NAMESPACE::obj> &b)
-		  {
-			  val |= 1;
-		  }))
+	auto got=f.get();
+
+	if (got)
 	{
-		val |= 2;
+		auto &[a, b] = *got;
+
+		typedef std::enable_if_t<std::is_same_v
+					 <decltype(a),
+					  LIBCXX_NAMESPACE
+					  ::ref<LIBCXX_NAMESPACE::obj>>> a_t;
+
+		typedef std::enable_if_t<std::is_same_v
+					 <decltype(b),
+					  LIBCXX_NAMESPACE
+					  ::ref<LIBCXX_NAMESPACE::obj>>> b_t;
+		val=1;
 	}
 	return val;
 }
@@ -25,7 +34,7 @@ int main()
 
 	auto captured=LIBCXX_NAMESPACE::make_weak_capture(a, b);
 
-	if (Foo(captured) != 3)
+	if (Foo(captured) != 1)
 	{
 		std::cerr << "Test 1 failed" << std::endl;
 
@@ -37,6 +46,34 @@ int main()
 	if (Foo(captured) != 0)
 	{
 		std::cerr << "Test 2 failed" << std::endl;
+
+		exit(1);
+	}
+
+	a=LIBCXX_NAMESPACE::ref<LIBCXX_NAMESPACE::obj>::create();
+	b=LIBCXX_NAMESPACE::ref<LIBCXX_NAMESPACE::obj>::create();
+
+	if (Foo(captured))
+	{
+		std::cerr << "Test 3 failed" << std::endl;
+
+		exit(1);
+	}
+
+	captured=LIBCXX_NAMESPACE::make_weak_capture(a, b);
+
+	if (Foo(captured) != 1)
+	{
+		std::cerr << "Test 4 failed" << std::endl;
+
+		exit(1);
+	}
+
+	a=b;
+
+	if (Foo(captured) != 0)
+	{
+		std::cerr << "Test 5 failed" << std::endl;
 
 		exit(1);
 	}
